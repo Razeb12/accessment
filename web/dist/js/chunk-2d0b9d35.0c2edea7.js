238c82aad230abe129f80fcc9dd373f53852eed4
Razeb Enyi <enyirazeb@gmail.com>
2022-02-05 13:39:21 +0100

    Add files via upload

---
+++ web/dist/js/chunk-2d0b9d35.0c2edea7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b9d35"],{3530:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},u=[],r={},s=r,a=t("2877"),i=Object(a["a"])(s,c,u,!1,null,"45c3b8e0",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b9d35.0c2edea7.js.map
